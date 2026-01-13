9bd8e72cb81f63888bc72c26074310d4a34f693f
dev-AndyMarcelo <161630733+dev-AndyMarcelo@users.noreply.github.com>
2026-01-13 17:50:26 -0500

    Create main.java

---
+++ 04-class-object/proyectoFinal/main.java
@@ -0,0 +1,10 @@
+package ProyectoFinal;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount bankAccount =
+                new BankAccount("Juan Perez", 10000.0);
+        BankApp bankApp = new BankApp(bankAccount);
+        bankApp.start();
+    }
+}
