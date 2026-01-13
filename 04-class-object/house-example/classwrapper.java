5c5577f131ec454ffce83ea92d5c10ce5787d845
dev-AndyMarcelo <161630733+dev-AndyMarcelo@users.noreply.github.com>
2026-01-13 17:47:11 -0500

    Add ClassWrapper with main method and examples

---
+++ 04-class-object/house-example/classwrapper.java
@@ -0,0 +1,15 @@
+package HouseExample;
+
+public class ClassWrapper {
+    public static void main(String[] args) {
+        int number = 10;
+        Integer number2 = Integer.valueOf(30);
+        Integer number3 = 40;
+
+        System.out.println(number2);
+        if(true){
+            Double size = 30.0;
+        }
+
+    }
+}
