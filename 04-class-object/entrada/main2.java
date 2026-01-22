778d61ce164cbc4e3ef5e056e3ea5770d8d647cf
dev-AndyMarcelo <161630733+dev-AndyMarcelo@users.noreply.github.com>
2026-01-21 22:45:24 -0500

    Create main2.java

---
+++ 04-class-object/entrada/main2.java
@@ -0,0 +1,12 @@
+package puntoDeEntrada;
+
+public class MainApp2 {
+    public static void main(String[] args) {
+        MainApp2 app = new MainApp2();   // Creo una instancia de la clase
+        app.sayHello();                // Llamo al método de instancia
+    }
+
+    public void sayHello() {
+        System.out.println("Hola, soy la aplicación principal");
+    }
+}
