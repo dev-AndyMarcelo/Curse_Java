aab59377c7a8085d8451210335cad37b993a2b26
dev-AndyMarcelo <161630733+dev-AndyMarcelo@users.noreply.github.com>
2026-01-21 22:44:58 -0500

    Create CarApp class with main method and colorPatente

---
+++ 04-class-object/entrada/carapp.java
@@ -0,0 +1,12 @@
+package puntoDeEntrada;
+
+public class CarApp {
+    private static String patente;
+    public static void main(String[] args) {
+
+    }
+
+    public static void colorPatente( String color){
+
+    }
+}
